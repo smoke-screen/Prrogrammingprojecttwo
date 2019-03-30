79254670961cdbc1dda25032b5f53cd90af64170
Burton <burtonkeck@outlook.com>
2019-03-30 09:12:58 -0400

    finished

---
+++ Assets/HealthDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour {
+    private int health = 5;
+    public Text healthText;
+
+    void update()
+    {
+        healthText.text = "HEALTH : " + health;
+        if (Input.GetKeyDown(KeyCode.Space)){
+            health--;
+        }
+    }
+
+}
---
+++ Assets/examplescript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class examplescript : MonoBehaviour
+{
+        public void Hello()
+    {
+     Debug.Log("Hello, my name is Burton !");
+    }
+}
